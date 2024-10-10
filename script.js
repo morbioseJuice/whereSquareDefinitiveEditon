b1c51f85c6f4b2ac4d5ea795fecd2f4cd92e0754
morbioseJuice <willjbriggs7@gmail.com>
2024-10-10 11:33:32 -0600

    Added buttons, centered canvas with border and color, better font, no weird teleportation, space reset, timer, win condition.

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ document.body.onkeydown = function(e) { //checks for pressed keys
         case 83: // S 
             player.keys[3] = true;
             break;
+        case 32: // Space
+            player.kill();
+            break;
     }
 }
 
@@ -61,30 +64,34 @@ var difficultyID = {
 }
 
 var gamemodes = {
-    sizemod: true, // When eating enemies, increases size by their size
+    sizemod: false, // When eating enemies, increases size by their size
     halfdamage: false, // When getting hit, you lose only half points *TRASH*
     mousefollow: true, // Square follows mouse. Intended gameplay.
     difficulty: difficultyID.normal, // formula: 1 to 50 size for alls squares, plus POINTS divided by DIFFICULTY (increases size the smaller the number is.)
     nocolor: false, //does not tell you what you can eat
     mod: {
         sizemod() {
-            if (this.sizemods)  
-                this.sizemod = false;
-            else {
-                this.sizemod = true;
+            if (gamemodes.sizemod) {
+                gamemodes.sizemod = false;
+                document.getElementById("sizemod").innerHTML = "Off";
+            } else {
+                gamemodes.sizemod = true;
+                document.getElementById("sizemod").innerHTML = "On"
             }
         },
         nocolor() {
-            if (gamemodes.nocolor)  
+            if (gamemodes.nocolor) {  
                 gamemodes.nocolor = false;
-            else {
+                document.getElementById("colormod").innerHTML = "Off";
+            } else {
                 gamemodes.nocolor = true;
+                document.getElementById("colormod").innerHTML = "On";
             }
         }
     }
 }
 
-var player = {
+var player = {  
     x: 250,
     y: 250,
     size: 10,
@@ -93,29 +100,36 @@ var player = {
     s: 3,
     points: 0,
     keys: [false, false, false, false],
-    draw() {
+    draw() {    
         ctx.fillStyle = "blue";
         ctx.fillRect(this.x, this.y, this.w, this.h);
     },
+    kill() {
+        player.points = 0;
+        player.size = 10;
+        timestart = Date.now();
+        // player.x = 250;
+        // player.x = 250;
+    },
     update() {
-    player.draw();
-    if (gamemodes.mousefollow == false) {
-        if (this.keys[0]) {
-            this.x -= this.s;
-        } 
-        else if (this.keys[1]) {
-            this.x += this.s;
-        } 
-        if (this.keys[2]) {
-            this.y -= this.s;
-        } 
-        else if (this.keys[3]) {
-            this.y += this.s;
+        player.draw();
+        if (gamemodes.mousefollow == false) {
+            if (this.keys[0]) {
+                this.x -= this.s;
+            } 
+            else if (this.keys[1]) {
+                this.x += this.s;
+            } 
+            if (this.keys[2]) {
+                this.y -= this.s;
+            } 
+            else if (this.keys[3]) {
+                this.y += this.s;
+            }
+        } else {
+            this.x = mx;
+            this.y = my;
         }
-    } else {
-        this.x = mx;
-        this.y = my;
-    }
         this.size = 10 + this.points;
         this.w = this.size;
         this.h = this.size;
@@ -138,6 +152,33 @@ var RRcollide = function(x1, x2, y1, y2, w1, w2, h1, h2){ //rect-rect colliding
     }
 }
 
+timestart = Date.now();
+
+var timer = function() {
+    time = Date.now();
+    timeshown = time - timestart;
+
+    minutes = Math.floor(timeshown / 60000)
+
+    seconds = timeshown - minutes * 60000
+    seconds = Math.floor(seconds * 10) / 10000
+    seconds = Math.floor(seconds * 10) / 10
+    seconds = Math.floor(seconds) // comment out for decimal
+
+    // document.getElementById("timer").innerHTML = "Time: " + (Math.floor(timeshown / 100) /10);
+    
+    if (player.points < canvas.width) { // You win when you fill up the screen, stop timer
+
+        if (seconds < 10) {
+            document.getElementById("timer").innerHTML = "Time: " + minutes + ":0" + seconds
+        } else {
+            document.getElementById("timer").innerHTML = "Time: " + minutes + ":" + seconds
+        }
+    }
+
+    window.requestAnimationFrame(timer);
+}
+
 var update = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -153,23 +194,24 @@ var update = function() {
         fish[i][0] += fish[i][3];
         fish[i][1] += fish[i][4];
         if (RRcollide(fish[i][0], player.x, fish[i][1], player.y, fish[i][2], player.w, fish[i][2], player.h)) {
+            
             if (fish[i][2] < player.size) {
+                
                 if (gamemodes.sizemod) {
                     player.points += fish[i][2];
                 } else {
                     player.points++;
                 }
+
                 fish[i][0] = 0;
                 fish[i][1] = 0;
                 fish[i][2] = 0;
                 fish[i][3] = 0;
                 fish[i][4] = 0;
+
             } else {
                 if (gamemodes.halfdamage == false) {
-                    player.points = 0;
-                    player.size = 10;
-                    player.x = 250;
-                    player.x = 250;
+                    player.kill()
                 } else {
                     player.points /= 2;
                 }
@@ -205,3 +247,22 @@ var update = function() {
 }
 
 window.requestAnimationFrame(update);
+window.requestAnimationFrame(timer);
+
+// Things added:
+
+// Buttons for mods
+// Centered canvas, gave it border, and background color
+// Changed font
+// Player doesn't teleport for second when dead
+// Now can press space to reset
+// Added a timer
+// You win when you are the canvas size
+
+
+// To do:
+
+// Add a widescreen mode
+// Add a survival mode (survive as long as possble)
+// Balance difficulties
+// Add a way to change the speedrun points goal for when to pause the timer
